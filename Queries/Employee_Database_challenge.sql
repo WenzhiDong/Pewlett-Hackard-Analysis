6ec5091e7b336d28f8c4bf0bae8f2d52fe2a0169
Wenzhi Dong <61805097+WenzhiDong@users.noreply.github.com>
2021-02-07 14:59:26 -0500

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 -- creating Retirement_Titles
 SELECT e.emp_no, e.first_name, e.last_name,ti.title,ti.from_date,ti.to_date
 INTO Retirement_Titles
@@ -8,15 +9,11 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no ASC;
 
 
-
 -- creating unique_titles
 SELECT DISTINCT ON (emp_no) emp_no,first_name, last_name,title,from_date,to_date
 into unique_titles
 FROM retirement_titles 
 order by emp_no asc, to_date Desc
-
-
-
 
 
 -- creating retiring_titles
@@ -25,3 +22,18 @@ into retiring_titles
 from unique_titles
 group by title
 order by count(emp_no) Desc
+
+
+
+
+-- Deliverable 2
+select distinct on(e.emp_no) e.emp_no,e.first_name,e.last_name,e.birth_date,
+	de.from_date,de.to_date,ti.title
+into Mentorship_Eligibility
+from employees as e
+inner join dept_emp as de
+	on (e.emp_no = de.emp_no)
+inner join titles as ti
+	on (e.emp_no=ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no
